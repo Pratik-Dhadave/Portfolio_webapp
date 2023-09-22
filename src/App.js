47252950aef95f6950dd9a1e5b89b854525925e4
Pratik_Dhadave <pratik007ddd@gmail.com>
2023-09-23 01:31:14 +0530

    work section is done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import Header from './components/Header'
 import Home from './components/Home'
+import Work from './components/Work'
+
 
 const App = () => {
   return <>
   <Header />
   <Home />
+  <Work />
   </>
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,7 +11,7 @@ const NavContent = () =>(
     <h2>Pratik.</h2>
     <div>
         <a href="#home">Home</a>
-        <a href="#word">Word</a>
+        <a href="#work">Work</a>
         <a href="#timeline">Experience</a>
         <a href="#services">Services</a>
         <a href="#testimonail">Testimonial</a>
